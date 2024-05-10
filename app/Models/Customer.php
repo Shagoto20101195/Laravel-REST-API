9dd424e00c12e43c78dbb22fe79145e8bd33d6f7
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-05-10 12:16:56 +0600

    Customer and Receipt models

---
+++ app/Models/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    public function receipt()
+    {
+        return $this->hasMany(Receipt::class);
+    }
+}
